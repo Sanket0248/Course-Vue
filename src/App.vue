db2735a2b83f5217eb7dc8c8b53a86aebd7af9ec
Sanket0248 <sanketgarg2004@gmail.com>
2026-02-21 02:09:19 +0530

    defineProfs

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,18 @@
     
     <h2 class="text-2xl font-medium">All Courses</h2>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
-      <CourseItem v-for="i in 3" :key="i"></CourseItem> <!--rendering the course item component 3 times to display 3 courses -->
+      <CourseItem 
+      v-for="i in 3" 
+      :key="i"
+      :title="`Course ${i}`"
+      :price="`$${i * 10}`"
+      :description="`This is the description for course ${i}`"
+      @click="alert(`You clicked on course ${i}`)">
+    </CourseItem> 
+    <!--rendering the course item component 3 times to display 3 courses 
+      The : is shorthand for v-bind:. It tells Vue "treat this value as JavaScript, not plain text".
+      @click is shorthand for v-on:click — it's Vue's way of listening to events. When the component is clicked, it runs whatever you pass to it.
+    -->
     </div>
 
 
--- src/components/CourseItem.vue
+++ src/components/CourseItem.vue
@@ -1,10 +1,25 @@
 <template>
     <div class="bg-white border border-grey-200 rounded-md">
 
-        <h3 class="p-4 text-xl font-medium border-b border-gray-200">Course Title</h3>
+        <h3 class="p-4 text-xl font-medium border-b border-gray-200">{{title}}</h3>
 
-        <p class="p-4  border-b border-gray-200">Price</p>
+        <p class="p-4  border-b border-gray-200">{{price}}</p>
 
-        <p class="p-4 text-medium">Course description goes here.</p>
+        <p class="p-4 text-medium">{{description}}</p>
+        <div class="flex justify-end p-4">
+            <button 
+                @click="$emit('click')"
+                class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Enroll Now</button>   
+        </div>
     </div>
 </template>
+
+<script setup>
+    defineProps({
+        title: String,
+        price: String,
+        description: String
+    })
+
+    defineEmits(['click'])
+</script>
