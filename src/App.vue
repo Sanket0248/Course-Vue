cfcce8347cd0dab864b2342cb204073fd0b0100d
Sanket0248 <sanketgarg2004@gmail.com>
2026-02-21 01:08:39 +0530

    Styling: v-for+import

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <h1 class="text-4xl font-medium">Course Hub</h1>
     
     <h2 class="text-2xl font-medium">All Courses</h2>
-    <CourseItem></CourseItem>
-    
+    <CourseItem v-for="i in 4" key="i"></CourseItem>
+
     <h2 class="text-2xl font-medium">Your Courses</h2>
 
 </template>
--- src/components/CourseItem.vue
+++ src/components/CourseItem.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="bg-white border border-grey-200 rounded-md">
-        <h3 class="p-4 text-medium border-b border-gray-200">Course Title</h3>
-        <p class="p-4 text-medium border-b border-gray-200">Price</p>
+
+        <h3 class="p-4 text-xl font-medium border-b border-gray-200">Course Title</h3>
+
+        <p class="p-4  border-b border-gray-200">Price</p>
+
         <p class="p-4 text-medium">Course description goes here.</p>
     </div>
 </template>
