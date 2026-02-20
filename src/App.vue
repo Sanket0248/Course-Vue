138a4c64e0d8876158605aab022c693c287c6f68
Sanket0248 <sanketgarg2004@gmail.com>
2026-02-21 00:36:08 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  
+    <h1 class="text-4xl font-medium">Course Hub</h1>
+    
+    <h2 class="text-2xl font-medium">All Courses</h2>
+    <CourseItem></CourseItem>
+    
+    <h2 class="text-2xl font-medium">Your Courses</h2>
+
+</template>
+
+
+<script setup>
+import CourseItem from './components/CourseItem.vue'
+</script>
---
+++ src/components/CourseItem.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-white border border-grey-200 rounded-md">
+        <h3 class="p-4 text-medium border-b border-gray-200">Course Title</h3>
+        <p class="p-4 text-medium border-b border-gray-200">Price</p>
+        <p class="p-4 text-medium">Course description goes here.</p>
+    </div>
+</template>
